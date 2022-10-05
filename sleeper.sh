c338c43f0651e269afce743ddc0ae55180e81300
Ariel Couso <ariel.couso@gmail.com>
2022-10-05 17:08:30 +0200

    Update sleeper.sh

--- sleeper.sh
+++ sleeper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#for ((i = 0 ; i < 15720 ; i++)); do
-for ((i = 0 ; i < 10 ; i++)); do
+for ((i = 0 ; i < 14520 ; i++)); do
+#for ((i = 0 ; i < 10 ; i++)); do
   #echo $i
   sleep 1 &
   wait
